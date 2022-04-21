f05bd5619fba276ab25744a99694ba4c1eaf5c87
Hbaghdadi1995 <Hasan@gowerlabs.co.uk>
2022-04-21 01:03:05 +0100

    Get group_id

--- +lumofile/check_app_data_for_layout.m
+++ +lumofile/check_app_data_for_layout.m
@@ -16,3 +16,26 @@ if(not(isfolder(layout_file_dir)))
     error("Unable to retrieve layout files: This machine either doesn't have the LumoView software installed, or was never had it used before.");
 end
 
+%% Get group_id
+
+if(isnumeric(group_id))
+    % Simple if value is numeric
+    group_id_num = group_id;
+else
+    %Otherwise, checking if value is a cell of char or char.
+    if(isa(group_id,'char'))
+        group_id_str = group_id;
+    elseif(isa(group_id,'string'))
+        group_id_str = group_id;
+    elseif(isa(group_id,'cell'))
+        if(isa(group_id{1}, 'char'))
+            group_id_str = group_id{1};
+        elseif(isa(group_id{1},'string'))
+            group_id_str = group_id;
+        else
+            error("Unable to retrieve layout files: group_id is not a char, cell with a char or numeric type.");
+        end
+    else
+        error("Unable to retrieve layout files: group_id is not a char, cell with a char or numeric type.");
+    end
+    
