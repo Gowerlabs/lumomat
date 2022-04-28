88541d5c6163456cff109b476cad90f41f39235d
Hbaghdadi1995 <Hasan@gowerlabs.co.uk>
2022-04-28 07:40:17 +0100

    Moved reqfield.m to a private function

--- +lumofile/read_lumo.m
+++ +lumofile/read_lumo.m
@@ -1041,21 +1041,6 @@ cs_det = struct('optode_idx', det_optode_idx);
 end
 
 
-% reqfield
-%
-% Get a required field from a structure, throwing an appropriate error if unavailable.
-%
-function fieldval = reqfield(s, name, lumodir_fn)
-
-if isfield(s, name)
-  fieldval = getfield(s, name);
-else
-  error('LUMO file (%s): required field %s missing from structure', lumodir_fn, name)
-end
-
-end
-
-
 % reqfieldci
 %
 % Get a required field from a structure, case insensitive on the field name, ithrowing an
